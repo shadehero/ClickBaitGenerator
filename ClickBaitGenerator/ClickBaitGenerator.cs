a73b86a4d440e4cd80c546d437c51365c2137bdc
Bartek Rybak <gotujcygana@gmail.com>
2019-04-26 04:41:11 -0700

    Add files via upload

---
+++ ClickBaitGenerator/ClickBaitGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HtmlAgilityPack;
+namespace ClickBaitGenerator
+{
+	public class ClickBaitGenerator
+	{
+		static readonly string SRC_URL = "http://linkbaitgenerator.com/marketing/index.php";
+
+		public static string GetClickBait()
+		{
+			var web = new HtmlWeb();
+			var doc = web.Load(SRC_URL);
+
+			var node = doc.DocumentNode.SelectSingleNode("//div/div/div/div/h2");
+			return node.InnerText;
+		}
+	}
+}
---
+++ ClickBaitGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace ClickBaitGenerator
+{
+	class MainClass
+	{
+		static string URL = "http://linkbaitgenerator.com/marketing/index.php";
+
+		public static void Main(string[] args)
+		{
+			for (int i = 0; i < 12; i++)
+			{
+				Console.WriteLine(ClickBaitGenerator.GetClickBait());			
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
